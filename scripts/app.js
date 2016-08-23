2cdc45084e56a34752420460a15ca6fbae9bdab6
14budig <nbudig@gmail.com>
2016-08-23 16:22:31 -0700

    completes the challenge with bonus

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,64 @@
 $(document).on("ready", function(){
+    var offset = 0;
+    var last = "";
+    $.ajax({
+      method:"GET",
+      url:"http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+      dataType: 'json',
+      success:populate,
+      error: errorMSG
+    });
+    function populate(json){
+      $('.gif-gallery').empty();
+      for(var i = 0; i < json.data.length; i++){
+        $('.gif-gallery').append('<a href="' + json.data[i].images.original.url
+        + '"> <img src="' + json.data[i].images.fixed_height_small.url + '" /> </a>')
+      }
+    }
+
+    function errorMSG(xhr, status, error){
+      console.log("Error: " + error);
+    }
+
+    $('form').on('submit', function(event){
+      event.preventDefault();
+      last = $('#q').val();
+      offset = 0;
+      $.ajax({
+        method:'GET',
+        url: 'http://api.giphy.com/v1/gifs/search?' + $(this).serialize(),
+        datatype: 'json',
+        success:populate,
+        error: errorMSG
+      });
+    });
+
+    $('#More').on('click', function(event){
+      event.preventDefault();
+      offset += 25;
+      var str;
+      if(last){
+        str = 'http://api.giphy.com/v1/gifs/search?q='+last+'&api_key=dc6zaTOxFJmzC&offset=' + offset;
+      }
+      else{
+        str = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&offset=' + offset;
+      }
+
+
+      $.ajax({metod:"GET",
+        url: str,
+        datatype:'json',
+        success:popNoDepop,
+        error:errorMSG
+      });
+      function popNoDepop(json){
+        for(var i = 0; i < json.data.length; i++){
+          $('.gif-gallery').append('<a href="' + json.data[i].images.original.url
+          + '"> <img src="' + json.data[i].images.fixed_height_small.url + '" /> </a>')
+        }
+      }
+
+
+    });
 
 });
